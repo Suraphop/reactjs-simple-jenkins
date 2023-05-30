324b3b5fd5428df6f74910718b49f9edc15ced1e
suraphop <mic@admin.com>
2023-05-30 11:52:33 +0700

    edit file

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -4,4 +4,4 @@ echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
-cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
+cd jenkins/build/ && docker-compose -f docker-compose-prod.yml build --no-cache
